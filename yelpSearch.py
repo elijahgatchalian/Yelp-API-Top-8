62916c5fb55a5b7cda0cb0ba4b8f601192b9e508
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-05-02 16:51:09 -0700

    Add files via upload

---
+++ yelpSearch.py
@@ -0,0 +1,83 @@
+#   Eli Gatchalian
+#   May 2, 2019
+
+import requests
+import urllib
+import tkinter as tk
+from geocoder import ip
+from webbrowser import open_new_tab
+from PIL import ImageTk, Image
+from io import BytesIO
+
+#api set-up
+YELP_API_KEY = '' #You can get a unique api key from yelp.com/developers
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization':'bearer %s' % YELP_API_KEY}
+
+#creating window
+window = tk.Tk()
+
+#lists to track a restaurant's image url and yelp url
+images_url = []
+yelp_url = []
+
+#locating user's city
+city = ip('me').city
+
+def getBusinessInformation():
+    parameters = {'location': city, 
+                  'limit': 20, #get 20 places, output max 8
+                  'radius': 1000,
+                  'categories': 'Restaurants',
+                  'open_now': True,
+                  'price': (1,2,3),
+                  'sort_by': 'rating'}
+    response = requests.get(url = ENDPOINT, params = parameters, headers = HEADERS)
+    business_dict = response.json()
+    return business_dict
+        
+def onClick(pic):
+    open_new_tab(yelp_url[pic])
+
+def createImage(url,i,j):
+    raw_data = urllib.request.urlopen(url).read()
+    img = Image.open(BytesIO(raw_data))
+    img = img.resize((250,250), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(img)
+    label1 = tk.Label(window,image=image)
+    label1.grid(row=i,column=j)
+    images_url.append(image)
+
+def createButton(name, url, i, j, pic_num):
+    button = tk.Button(window, text = name, command = lambda idx = pic_num: onClick(idx))
+    button.grid(row=i,column=j,sticky='s')
+    yelp_url.append(url)
+
+def addInfoToWindow(business_data):
+    i = 0 #row
+    j = 0 #column
+    pic_num = 0
+    for biz in business_data['businesses']:
+        #Should include 8 businesses if they have valid image and yelp url
+        if (pic_num > 7):
+            return
+        if(biz['image_url'] == '' or biz['url'] == '' or pic_num > 7):
+            continue 
+        
+        createImage(biz['image_url'],i,j)
+        createButton(biz['name'],biz['url'],i,j,pic_num)
+        pic_num += 1
+        
+        if(j < 3):
+            j += 1
+        else:
+            j = 0
+            i += 1
+
+def main():
+    addInfoToWindow(getBusinessInformation())
+    window.title(str(len(images_url)) + " food places in " + city)
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
